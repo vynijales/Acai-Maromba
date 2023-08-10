96c34cb2f82a78cc906c75c755765c5a0d62aa23
vynijales <vynijales@gmail.com>
2023-08-10 14:12:54 -0300

    Atualizado classe Pedido

--- classes/Pedido.java
+++ classes/Pedido.java
@@ -7,17 +7,19 @@ public class Pedido {
     private Adicional[] adicionais;
     private boolean status;
     private double valor;
+    private static int totalPedidos;
 
     public Pedido(Produto principal, Adicional[] adicionals) {
         setPrincipal(principal);
         setAdicionals(adicionals);
         this.status = false;
+        totalPedidos++;
     }
     public void setPrincipal(Produto principal) {
-        this.principal = principal;
+        if (principal != null) {this.principal = principal;}
     }
     public void setAdicionals(Adicional[] adicionais) {
-        this.adicionais = adicionais;
+        if (adicionais != null) {this.adicionais = adicionais;}
     }
     public void setStatus(boolean status) {
         this.status = status;
@@ -31,5 +33,11 @@ public class Pedido {
     }
     public boolean getStatus() {
         return this.status;
+    }
+    public double getTotalValor() {
+        return this.valor;
+    }
+    public int getTotalPedidos() {
+        return totalPedidos;
     }
 }
